04dfab6a2a1e03482ed3769242d5d28c65eb0644
Mayara Viana <54589016+mayaraviana@users.noreply.github.com>
2021-07-20 20:09:53 -0300

    first commit

---
+++ meal-prep/src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+    <q-toolbar class="bg-brown-8 text-white shadow-2 rounded-borders">
+      <q-btn flat label="Meal Prep" />
+      <q-space />
+
+      <!--
+        notice shrink property since we are placing it
+        as child of QToolbar
+      -->
+      <q-tabs v-model="tab" shrink class="md">
+        <q-tab name="menu" label="menu" />
+        <q-tab name="signin" label="Sign in" />
+        <q-btn color="brown-5" name="join" label="Join" />
+      </q-tabs>
+      <q-btn flat round dense icon="menu" class="q-mr-sm lt-md" />
+    </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+})
+</script>
